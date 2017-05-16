bb9e19ab9c2da7f4394996dcf4e945bb43d9096e
yguo18 <yguoelect@hotmail.com>
2017-05-16 17:43:28 +0800

    add key board desigin project

---
+++ KeyBoard Desigin Inteface/Assets/Keyword.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+public class Keyword : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			if (EventSystem.current.currentSelectedGameObject != null) {
+				Debug.Log (EventSystem.current.currentSelectedGameObject.transform.Find ("Text").GetComponent<Text> ().text.ToString ());
+				GameObject.Find ("Canvas/Image/Text").GetComponent<Text> ().text += EventSystem.current.currentSelectedGameObject.transform.Find ("Text").GetComponent<Text> ().text.ToString ();
+			}
+		}
+	}
+}
